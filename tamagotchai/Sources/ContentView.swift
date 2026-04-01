bd46ac700ebd71bbb84632d84fa3fa36ba06f879
kenkaiii <buzzbeamaustralia@gmail.com>
2026-04-01 22:00:16 +0800

    Initial project setup: macOS SwiftUI app with SwiftLint & SwiftFormat

---
+++ tamagotchai/Sources/ContentView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Tamagotchai")
+            .font(.largeTitle)
+            .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tamagotchai/Sources/TamagotchaiApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct TamagotchaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
